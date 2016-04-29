2a1dd48891b54a4e899f85915493702ea13145c7
slightair <arksutite@gmail.com>
2016-04-29 22:38:43 +0900

    Support log in with GitHub

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.name = auth['info']['nickname']
+      user.oauth_token = auth['credentials']['token']
+    end
+  end
+end
