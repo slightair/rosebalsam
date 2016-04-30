1e22ad99523d29ada928aa5a5ff8f7b8ab753502
slightair <arksutite@gmail.com>
2016-04-30 18:27:23 +0900

    Change uid type in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430075016) do
+ActiveRecord::Schema.define(version: 20160430092053) do
 
   create_table "tokens", force: :cascade do |t|
     t.integer  "user_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160430075016) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "provider"
-    t.text     "uid"
+    t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
