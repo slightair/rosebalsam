72fdc6bfdff7d0cf008d35cf9588c53977cf936a
slightair <arksutite@gmail.com>
2016-04-30 16:55:41 +0900

    Create token model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430073317) do
+ActiveRecord::Schema.define(version: 20160430075016) do
+
+  create_table "tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tokens", ["token"], name: "index_tokens_on_token", unique: true
+  add_index "tokens", ["user_id"], name: "index_tokens_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
