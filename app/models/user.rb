a0df5830465a15e4e879d5c092c2f6ccde0bae07
slightair <arksutite@gmail.com>
2016-04-30 18:03:58 +0900

    Create user api token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,27 @@
+require 'securerandom'
+
 class User < ActiveRecord::Base
+  has_many :tokens
+
+  after_create :create_token
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
       user.name = auth['info']['nickname']
     end
+  end
+
+  def create_token
+    tokens.active.update_all(deleted_at: Time.now)
+    loop do
+      new_token = SecureRandom.uuid
+      return tokens.create(token: new_token) if Token.where(token: new_token).empty?
+    end
+  end
+
+  def token
+    tokens.active.first.try(:token)
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :tokens, :users
+
+  test "alice's token" do
+    alice = users(:alice)
+    assert_equal "TOKEN_ALICE_2", alice.token
+  end
+
+  test "bob's token" do
+    bob = users(:bob)
+    assert_equal "TOKEN_BOB_1", bob.token
+  end
+
+  test "create token after created user" do
+    tom = User.create(
+        name: "tom",
+        provider: "github",
+        uid: 300,
+    )
+    assert_not_nil tom.token
+  end
+
+  test "renew token when already user exist" do
+    alice = users(:alice)
+    prev_token = alice.tokens.active.first
+    assert_nil prev_token.deleted_at
+
+    alice.create_token
+
+    prev_token.reload
+    assert_not_nil prev_token.deleted_at
+    assert_not_equal "TOKEN_ALICE_2", alice.token
+  end
 end
