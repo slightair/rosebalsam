6096d1eff67c425e23d0adbe14dad66e93807efc
slightair <arksutite@gmail.com>
2016-05-03 21:35:42 +0900

    Return api token with callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_path
+
+    if client_code = params[:client]
+      if client = Client.find_by(code: client_code)
+        redirect_to client.callback_url_with current_user.token
+      else
+        render :nothing => true, :status => 404
+      end
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users, :clients
+
+  def setup
+    user = users(:alice)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+        provider: 'github',
+        uid: user.uid,
+        info: {name: user.name},
+    )
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+  end
+
+  test "GET /auth/github/callback" do
+    get :callback, provider: 'github'
+    assert_redirected_to root_path
+  end
+
+  test "GET /auth/github/callback?client=VALID_CODE" do
+    get :callback, provider: 'github', client: 'aaaaaaaaaaaaaaaaaaaa'
+    assert_redirected_to "iosApp://rosebalsam/callback?token=TOKEN_ALICE_2"
+  end
+
+  test "GET /auth/github/callback?client=INVALID_CODE" do
+    get :callback, provider: 'github', client: 'invalid'
+    assert_response 404
+  end
 end
