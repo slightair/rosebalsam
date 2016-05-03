ddc9ffdf8697c5251c7e2e3568cd18c12eda9113
slightair <arksutite@gmail.com>
2016-05-03 11:29:42 +0900

    Add client test

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,10 @@
 class Client < ActiveRecord::Base
   scope :active, -> { where(deleted_at: nil) }
+
+  def callback_url_with(token)
+    params = {
+        token: token
+    }
+    "#{callback_url}?#{params.to_param}"
+  end
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :clients
+
+  test "active clients" do
+    active_clients = Client.active
+    assert_equal 2, active_clients.size
+
+    client_names = active_clients.map(&:name)
+    assert_includes client_names, "iOSApp"
+    assert_includes client_names, "AndroidAppB"
+  end
+
+  test "callback_url_with" do
+    client = clients(:ios_app)
+    token = 'aaaaaaaaaaaaaaaaaaaa'
+    assert_equal 'iosApp://rosebalsam/callback?token=aaaaaaaaaaaaaaaaaaaa', client.callback_url_with(token)
+  end
 end
